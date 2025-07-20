15e3d03df9d75049a2eb4be565d7050b880c13fd
Christian Sutton <chrsut2005@gmail.com>
2025-07-19 22:41:02 -0700

    Ability to quit graffiti event

--- Assets/Scripts/Graffiti/GraffitiEvent.cs
+++ Assets/Scripts/Graffiti/GraffitiEvent.cs
@@ -1,5 +1,6 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class GraffitiEvent : MonoBehaviour
@@ -7,15 +8,35 @@ public class GraffitiEvent : MonoBehaviour
   public GameObject mainCamera;
   public GameObject graffitiCam;
   public GameObject mainPlayer;
+  private float originalSpeed;
 
   public void StartEvent()
   {
     Debug.Log("GraffitiEvent: Locking player");
     PlayerMovement playerMovement = mainPlayer.GetComponent<PlayerMovement>();
-    float playerSpeed = playerMovement.movementSpeed;
+    originalSpeed = playerMovement.movementSpeed;
     playerMovement.movementSpeed = 0;
-    Debug.Log("GraffitiEvent: Changing camera position");
+    Debug.Log("GraffitiEvent: Changing camera position to grafiti");
     CinemachineCamera freeCamera = graffitiCam.GetComponent<CinemachineCamera>();
     freeCamera.Priority = 2;
+  }
+
+  public void EndEvent()
+  {
+    Debug.Log("GraffitiEvent: Unlocking player");
+    PlayerMovement playerMovement = mainPlayer.GetComponent<PlayerMovement>();
+    playerMovement.movementSpeed = originalSpeed;
+    Debug.Log("GraffitiEvent: Changing camera position to original");
+    CinemachineCamera freeCamera = graffitiCam.GetComponent<CinemachineCamera>();
+    freeCamera.Priority = 0;
+    Destroy(gameObject);
+  }
+
+  void Update()
+  {
+    if (Keyboard.current.escapeKey.wasPressedThisFrame)
+    {
+      EndEvent();
+    }
   }
 }
