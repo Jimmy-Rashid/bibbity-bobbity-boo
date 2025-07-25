d8b66b191b92c99fc63a19fa4586ce6627ac191a
Jimmy Rashid <jimmyrashid22@gmail.com>
2025-07-25 16:52:44 -0700

    Added script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject Menu;
-    public GameObject StartButton;
-    public GameObject EndButton;
-
-    void Start()
-    {
-
-    }
+    public GameObject menu;
+    public GameObject startButton;
+    public GameObject endButton;
+    public String sceneName;
+    public bool gameStarted = false;
 
     void Update()
     {
+        if (gameStarted == true)
+        {
+            menu.SetActive(false);
+            startButton.SetActive(false);
+            endButton.SetActive(true);
 
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
     }
 }
